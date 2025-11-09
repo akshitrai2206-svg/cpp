10832389f0f98291eb7ec981cdf5d4a004f5efc7
akshitrai2206-svg <akshitrai2206@gmail.com>
2025-11-09 21:06:24 +0530

    Update while.cpp

--- while.cpp
+++ while.cpp
@@ -1,37 +1,37 @@
-// Question 1
-// Write a C++ program to print from 10 to 1
+Question 1
+Write a C++ program to print from 10 to 1
 
-// #include <iostream>
-// using namespace std;
-// int main(){
+#include <iostream>
+using namespace std;
+int main(){
 
-//     int num = 10;
-//     while (num>=1){
-//         cout << num;
-//         num--;
-//     }
+    int num = 10;
+    while (num>=1){
+        cout << num;
+        num--;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
-// Question 2
-// Write a C++ program to print the series of odd numbers which include 1 to 20
-// #include <iostream>
-// using namespace std;
-// int main(){
+Question 2
+Write a C++ program to print the series of odd numbers which include 1 to 20
+#include <iostream>
+using namespace std;
+int main(){
 
-//     int num = 1;
+    int num = 1;
 
-//     while(num<=20){
-//         num++;
-//         if(num % 2 != 0){
-//             cout << num;
-//         }
-//     }
-//     return 0;
-// }
+    while(num<=20){
+        num++;
+        if(num % 2 != 0){
+            cout << num;
+        }
+    }
+    return 0;
+}
 
 
 
