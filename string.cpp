848f74e7586c2b59c0f6da18b902a558102d2af5
akshitrai2206-svg <akshitrai2206@gmail.com>
2025-11-09 21:04:02 +0530

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -1,63 +1,63 @@
-// Question 1
-// Write a C++ program to calculate the length of a string
+Question 1
+Write a C++ program to calculate the length of a string
 
-// #include <iostream>
-// #include <string.h>
-// using namespace std;
-// int main(){
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
 
-//     string firstname = "David";
-//     cout << "The length of the string is: " << firstname.length();
+    string firstname = "David";
+    cout << "The length of the string is: " << firstname.length();
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
-// // Question 2
-// // Write a C++ program to accept a string from user and display it
+// Question 2
+// Write a C++ program to accept a string from user and display it
 
-// #include <iostream>
-// #include <string.h>
-// using namespace std;
-// int main(){
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
 
-//     string first_name;
+    string first_name;
 
-//     cout << "Enter your name: ";
-//     cin >> first_name;
+    cout << "Enter your name: ";
+    cin >> first_name;
     
-//     return 0;
-// }
+    return 0;
+}
 
 
 
-// // Question 3
-// // Write a C++ program to accept a string from user and copy it to another string
+// Question 3
+// Write a C++ program to accept a string from user and copy it to another string
 
-// #include <iostream>
-// #include <string.h>
-// using namespace std;
-// int main(){
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
 
-//     string first_name;
-//     cout <<"Enter you first name: ";
-//     cin >> first_name;
+    string first_name;
+    cout <<"Enter you first name: ";
+    cin >> first_name;
 
-//     string last_name;
-//     cout << "Enter your last name: ";
-//     cin >> last_name;
+    string last_name;
+    cout << "Enter your last name: ";
+    cin >> last_name;
 
-//     string full_name = first_name + " " + last_name;
-//     cout << "Your full name is: " << full_name;
+    string full_name = first_name + " " + last_name;
+    cout << "Your full name is: " << full_name;
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
-// // Question 4
-// // Write a C++ program take a string from user and count the words
+// Question 4
+// Write a C++ program take a string from user and count the words
 
 #include <iostream>
 #include <cstring>   
